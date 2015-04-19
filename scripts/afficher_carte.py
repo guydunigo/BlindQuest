1cc1903df689306affb9e16a09c26b08c53de2f1
Guillaume GONI <g4goni@enib.fr>
2015-04-20 01:58:39 +0200

    Ajout d'une carte de test pour l'affichage et de implémentation de la fonction afficher la carte et réglage des couleurs.

--- scripts/afficher_carte.py
+++ scripts/afficher_carte.py
@@ -19,22 +19,23 @@ DEBUT = 98
 FIN = 99
 
 
-COULEUR_PLAINE = ('c4B', (0,200,0, 0)*4)					# VERT CLAIR
-COULEUR_FORET = ('c4B', (0,0,100, 0)*4)					# VERT FONCE
-COULEUR_CAVERNE = ('c4B', (100,0,100, 0)*4)				# MARRON
-COULEUR_EAU = ('c4B', (0,0,255, 0)*4)					# BLEU
-COULEUR_CHATEAU = ('c4B', (200,200,200, 0)*3, (100, 100, 100, 0))	# GRIS CAIR DEGRADE
-COULEUR_SENTIER = ('c4B', (255,0,255, 0)*4)				# VERT MARRON
-COULEUR_PONT = ('c4B', (0,0,255, 0)*4)
-COULEUR_SABLE = ('c4B', (0,0,255, 0)*4)
-COULEUR_MONTAGNE = ('c4B', (0,0,255, 0)*4)
-COULEUR_MOB = ('c4B', (0,0,255, 0)*4)
-COULEUR_BOSS = ('c4B', (0,0,255, 0)*4)
-COULEUR_BOSS_FINAL = ('c4B', (255,0,0, 0)*4)
-COULEUR_BONUS = ('c4B', (0,0,255, 0)*4)
-COULEUR_DEBUT = ('c4B', (0,0,255, 0)*4)
-COULEUR_FIN = ('c4B', (100,100,100, 0)*4)
+COULEUR_PLAINE = ('c4B', (150,175,10,0)*4)				# VERT CLAIR
+COULEUR_FORET = ('c4B', (0,100,0,0)*4)					# VERT FONCE
+COULEUR_CAVERNE = ('c4B', (100,50,0,0)*4)				# MARRON
+COULEUR_EAU = ('c4B', (0,0,255,0)*4)					# BLEU
+COULEUR_CHATEAU = ('c4B', (150,150,150,0)*4)				# GRIS CAIR
+COULEUR_SENTIER = ('c4B', (10,200,0,0)*4)				# VERT MARRON
+COULEUR_PONT = ('c4B', (100,50,0,0)*2 + (200,100,0,0)*2)		# DEGRADE DE MARRON
+COULEUR_SABLE = ('c4B', (200,200,0,0)*4)				# JAUNE
+COULEUR_MONTAGNE = ('c4B', (50,10,0,0)*4)				# MARRON FONCE
+COULEUR_MOB = ('c4B', (255,255,0,0)*2 + (200,70,0,0)*2)			# DEGRADE JAUNE - ORANGE
+COULEUR_BOSS = ('c4B', (200,200,0,0)*2 + (255,0,0,0)*2)			# DEGRADE ORANGE - ROUGE
+COULEUR_BOSS_FINAL = ('c4B', (255,0,0,0)*2 + (0,0,0,0)*2)		# DEGRADE ROUGE - NOIR
+COULEUR_BONUS = ('c4B', (200,0,200,0)*4)				# mauve (ou 'mave' selon la provenance)
+COULEUR_DEBUT = ('c4B', (255,255,255,0)*3 + (0,0,0,0))			# DEGRADE BLANC - NOIR
+COULEUR_FIN = ('c4B', (0,0,0,0)*3 + (255,255,255,255))			# DEGRADE NOIR - BLANC
 
+COULEURS = {PLAINE:COULEUR_PLAINE, FORET:COULEUR_FORET, CAVERNE:COULEUR_CAVERNE, EAU:COULEUR_EAU, CHATEAU:COULEUR_CHATEAU, SENTIER:COULEUR_SENTIER, PONT:COULEUR_PONT, SABLE:COULEUR_SABLE, MONTAGNE:COULEUR_MONTAGNE, MOB:COULEUR_MOB, BOSS:COULEUR_BOSS, BOSS_FINAL:COULEUR_BOSS_FINAL, BONUS:COULEUR_BONUS, DEBUT:COULEUR_DEBUT, FIN:COULEUR_FIN}
 
 class Carte :
 	def __init__(self, carte = None) :
@@ -56,6 +57,8 @@ class Carte :
 			self._cx = 0
 		elif new_cx < 0 :
 			self._cx = self.nb_colonnes - 1
+		else :
+			self._cx = new_cx
 	cx = property(_get_cx, _set_cx)
 	
 	def _get_cy(self) :
@@ -67,6 +70,8 @@ class Carte :
 			self._cy = 0
 		elif new_cy < 0 :
 			self._cy = self.nb_lignes - 1
+		else :
+			self._cy = new_cy
 	cy = property(_get_cy, _set_cy)
 
 	def ouvrir_carte(self, carte) :
@@ -75,6 +80,7 @@ class Carte :
 		try :
 			with open("../cartes/carte_" + carte + ".txt", "r") as fichier_carte :
 				self.charger_carte(fichier_carte)
+				self.nom = carte
 
 		except TypeError :
 			if carte != None :
@@ -87,7 +93,9 @@ class Carte :
 
 
 	def creer(self) :
+		"""Demande le nom, le nombre de colonnes et de lignes et crée une carte remplie de plaines"""
 
+		self.nom = input("Entrez le nom de la nouvelle carte :")
 		self.nb_lignes = int(input("Entrez le nombre de lignes de la carte : "))
 		self.nb_colonnes = int(input("Entrez le nombre de colonnes de la carte : "))
 
@@ -99,12 +107,13 @@ class Carte :
 				self.carte[i].append(00)
 
 	def charger_carte(self, fichier_carte) :
+		"""Charge le contenu d'un fichier carte dans une liste et met à jour en conséquence le nombre de colonnes et de lignes."""
 
 		self.carte = []
 		
 		fichier = fichier_carte.read().split("\n") 
 		for i in range(len(fichier)) :
-			self.carte.append([a for a in fichier[i].split() if a != ''])
+			self.carte.append([int(a) for a in fichier[i].split() if a != ''])
 		del(self.carte[len(self.carte) - 1])
 		
 		self.nb_lignes = len(self.carte)
@@ -112,6 +121,8 @@ class Carte :
 		print(self.carte,self.nb_colonnes, self.nb_lignes)
 
 	def event_init(self) :
+		"""Crée les événements clavier."""
+
 		@self.window.event
 		def on_key_press(symbol, modifiers) :
 			if symbol == pyglet.window.key.RIGHT :
@@ -127,14 +138,17 @@ class Carte :
 			self.afficher()
 
 			if symbol in (pyglet.window.key.DOWN, pyglet.window.key.UP, pyglet.window.key.LEFT, pyglet.window.key.RIGHT) :
-				pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [curs[0] * taille_case, curs[1] * taille_case, (curs[0] + 1) * taille_case, curs[1] * taille_case, (curs[0] + 1) * taille_case, (curs[1] + 1) * taille_case, curs[0] * taille_case, (curs[1] + 1) * taille_case]), ('c4B', (255, 0, 0, 0)*4))
+				pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [self.cx * TAILLE_CASE, self.cy * TAILLE_CASE, (self.cx + 1) * TAILLE_CASE, self.cy * TAILLE_CASE, (self.cx + 1) * TAILLE_CASE, (self.cy + 1) * TAILLE_CASE, self.cx * TAILLE_CASE, (self.cy + 1) * TAILLE_CASE]), ('c4B', (255, 0, 0, 0)*4))
 
 	def afficher(self) :
-		for i in range(len(self.carte)) :
-			for j in self.carte[i] :
-				pass
+		"""Afficher la carte selon le code couleurs définit au début."""
+
+		for i in range(self.nb_lignes) :
+			for j in range(self.nb_colonnes) :
+				pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [j * TAILLE_CASE, (self.nb_lignes - i) * TAILLE_CASE, (j + 1) * TAILLE_CASE, (self.nb_lignes - i) * TAILLE_CASE, (j + 1) * TAILLE_CASE, ((self.nb_lignes - i) - 1) * TAILLE_CASE, j * TAILLE_CASE, ((self.nb_lignes - i) - 1) * TAILLE_CASE]), COULEURS[self.carte[i][j]])
 
 if __name__ == "__main__" :
-	carte = Carte("defaut")
+
+	carte = Carte("test")
 
 	pyglet.app.run()
