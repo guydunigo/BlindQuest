2e53afd1415a98cbcb0d08de362209d780d892eb
Guillaume GONI <g4goni@enib.fr>
2015-05-08 12:38:05 +0200

    Ajout du test de l'existence de py3 dans le script shell.

--- blindquest.sh
+++ blindquest.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+#Création du dossier de logs
 mkdir log 2> /dev/null
 
-case $1 in
+#Si aucun argument n'est donné, on teste si python 3 est présent, sinon on utilise python 2
+if [ $1="" ] & [ -z $(command -v "python3") ]
+then
+echo "Python version 3 non trouvé, utilisation de la version 2."
+arg='-2'
+else
+arg=$1
+fi
+
+case $arg in
 	""|-3|-3d|--python3|-d|--debug)
 		echo "
 
@@ -40,7 +50,7 @@ Type=Application" > blindquest.desktop
 	--map-editor|-m)
 		python3 ./scripts/editeur_cartes.py
 	;;
-	--help|-h)
+	*)
 		echo "Jeu BlindQuest,
 Un jeu d'aventure exclusivement basé sur du son
 
@@ -52,16 +62,14 @@ OPTIONS :
 			-2d	Utilise python2 et affiche le fichier d'erreurs à la fin.
 	--python3	-3	Utilise python3 (par défaut) pour exécuter le jeu.
 	-d -3d --debug 		Utilise python3 et affiche le fichier d'erreurs à la fin.
- 	--install	-i	Crée un fichier .desktop du jeu.
+ 	--install	-i	Crée un raccourci .desktop du jeu.
 	--map-editor	-m	Ouvre l'éditeur de carte.
-	--help		-h	Affiche l'aide.
+	--help		-h	Affiche cette aide.
 "
-	;;
-	*)
 	;;
 esac
 
-case $1 in
+case $arg in
 	-d|-3d|--debug)
 		cat log/errors_py3.log 
 	;;
